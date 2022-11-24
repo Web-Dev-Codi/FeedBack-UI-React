cee21a2d006ba298683052de9c47e8fc7de463a3
unknown <desiredshellstate@gmail.com>
2022-11-24 11:05:59 +0100

    added new favicon png

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = 'Blog Post';
-  const body = ' This is awesome'
-  const comments = [
-    { id: 1, text: 'comment one' },
-    { id: 2, text: 'comment two' },
-    { id: 3, text: 'comment three' },
-    { id: 4, text: 'comment four' },
-    { id: 5, text: 'comment five' },
-    { id: 6, text: 'comment six' }
-  ];
-
-
-  const loading = false;
-  const showComments = false;
-
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
 
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
+    <>
+      <Header text="Try Hard" />
+      <div className="container">
+        <h1>Keep Going</h1>
 
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((id, index) => {
-            return <ul key={index}><li>{id.text}</li></ul>;
-          })}
-        </ul>
       </div>
-    </div>
+    </>
   )
 }
 
