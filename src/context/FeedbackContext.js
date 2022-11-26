e12387ccc4bcd3de536610582a228bb3c7a5b2e2
unknown <desiredshellstate@gmail.com>
2022-11-26 20:55:27 +0100

    Need to add actual backend instead of json-server to display the page live

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -24,14 +24,25 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Deletes feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete this my good friend?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
   // Update Feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
   }
 
   // add feedback
